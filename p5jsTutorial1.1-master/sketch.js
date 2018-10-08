ebb9ca97c262fe86710ea802894c2ddfccab9a94
DieDanishEmpire <43802326+DieDanishEmpire@users.noreply.github.com>
2018-10-08 12:26:49 +0800

    Add files via upload

---
+++ p5jsTutorial1.1-master/sketch.js
@@ -0,0 +1,24 @@
+function setup()
+{
+  //create a drawing service 700px wide, 500px tall
+  createCanvas(700,500);
+  
+}
+
+function draw()
+{
+  //create a background using RGB values
+  background(204,0,0)
+
+  // change the paint brush to a specific color according to RGB values
+  fill(1,76,153); 
+
+  fill(255,255,255);
+  // create a rectangle at x,y coordinates 10,10 with length x height at 100 x 50
+  rect(-1,-350,1000,500);
+    
+  rect(-1,300,750,200);
+
+
+
+}
